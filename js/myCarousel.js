b6573a4d908e8c7266e94427d79ebc7f48d2dcbe
digor <idabija@gmail.com>
2019-05-08 16:26:53 +0300

    add lesson 5

---
+++ js/myCarousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.container').carousel({
+        num: 3,
+        maxWidth: 850,
+        maxHeight: 600,
+        distance: 250,
+        scale: 0.8,
+        animationTime: 1000,
+        showTime: 4000
+    });
+});
